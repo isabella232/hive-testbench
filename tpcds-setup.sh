1d180b5b7b16c5077f028747d2b9348496181afa
Harshavardhana <harsha@minio.io>
2019-07-26 14:13:00 -0700

    partitioned table LOCATION is relative to text table

--- tpcds-setup.sh
+++ tpcds-setup.sh
@@ -99,11 +99,12 @@ REDUCERS=$((test ${SCALE} -gt ${MAX_REDUCERS} && echo ${MAX_REDUCERS}) || echo $
 for t in ${DIMS}
 do
 	COMMAND="$HIVE  -i settings/load-partitioned.sql -f ddl-tpcds/bin_partitioned/${t}.sql \
-	    --hivevar DB=tpcds_bin_partitioned_${FORMAT}_${SCALE} --hivevar SOURCE=tpcds_text_${SCALE} \
+	    --hivevar DB=tpcds_bin_partitioned_${FORMAT}_${SCALE} \
+            --hivevar SOURCE=tpcds_text_${SCALE} \
             --hivevar SCALE=${SCALE} \
 	    --hivevar REDUCERS=${REDUCERS} \
 	    --hivevar FILE=${FORMAT} \
-            --hivevar LOCATION=${DIR}/partitioned/${SCALE}"
+            --hivevar LOCATION=${DIR}"
 	echo -e "${t}:\n\t@$COMMAND $SILENCE && echo 'Optimizing table $t ($i/$total).'" >> $LOAD_FILE
 	i=`expr $i + 1`
 done
@@ -113,9 +114,12 @@ do
 	COMMAND="$HIVE  -i settings/load-partitioned.sql -f ddl-tpcds/bin_partitioned/${t}.sql \
 	    --hivevar DB=tpcds_bin_partitioned_${FORMAT}_${SCALE} \
             --hivevar SCALE=${SCALE} \
-	    --hivevar SOURCE=tpcds_text_${SCALE} --hivevar BUCKETS=${BUCKETS} \
-	    --hivevar RETURN_BUCKETS=${RETURN_BUCKETS} --hivevar REDUCERS=${REDUCERS} --hivevar FILE=${FORMAT} \
-            --hivevar LOCATION=${DIR}/partitioned/${SCALE}"
+	    --hivevar SOURCE=tpcds_text_${SCALE} \
+            --hivevar BUCKETS=${BUCKETS} \
+	    --hivevar RETURN_BUCKETS=${RETURN_BUCKETS} \
+            --hivevar REDUCERS=${REDUCERS} \
+            --hivevar FILE=${FORMAT} \
+            --hivevar LOCATION=${DIR}"
 	echo -e "${t}:\n\t@$COMMAND $SILENCE && echo 'Optimizing table $t ($i/$total).'" >> $LOAD_FILE
 	i=`expr $i + 1`
 done
